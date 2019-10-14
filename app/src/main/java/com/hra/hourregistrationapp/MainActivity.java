81d3c0a05ee89c196f059bea3339cc847c2b4c64
Leonard Bos <leonard442@hotmail.com>
2019-10-14 12:50:21 +0200

    Initial commit

---
+++ app/src/main/java/com/hra/hourregistrationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hra.hourregistrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
